989d56204d6ac49854195e11ea5230d18c3106c1
ilhanmanzis <ilhanmanzis1207@gmail.com>
2025-07-05 21:33:14 +0700

    perbaikan ukuran icon di halaman kontak untuk tampilan mobile

--- resources/views/public/kontak.blade.php
+++ resources/views/public/kontak.blade.php
@@ -21,7 +21,7 @@
                 <div class="md:w-1/2 w-full pl-0 md:pl-10 ">
                     <span class="text-blue-950 text-3xl font-bold">Kontak</span>
                     {{-- email --}}
-                    @if (!empty($kontak['kontak']))
+                    @if (!empty($kontak['email']))
                         <div class="flex justify-start items-center mt-5">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                                 class="size-10 text-blue-950">
@@ -38,7 +38,7 @@
                     {{-- alamat --}}
                     <div class="flex justify-start items-center mt-5">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="size-12 text-blue-950">
+                            class="size-18 md:size-12 text-blue-950">
                             <path fill-rule="evenodd"
                                 d="M8.161 2.58a1.875 1.875 0 0 1 1.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0 1 21.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 0 1-1.676 0l-4.994-2.497a.375.375 0 0 0-.336 0l-3.868 1.935A1.875 1.875 0 0 1 2.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437ZM9 6a.75.75 0 0 1 .75.75V15a.75.75 0 0 1-1.5 0V6.75A.75.75 0 0 1 9 6Zm6.75 3a.75.75 0 0 0-1.5 0v8.25a.75.75 0 0 0 1.5 0V9Z"
                                 clip-rule="evenodd" />
